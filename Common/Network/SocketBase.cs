baada6fb2c6aef081d85ea98fa7e6edc23b87673
yujin <uj0791navercom@skuniv.ac.kr>
2025-02-17 14:57:13 +0900

    [REFACTOR] SocketBase
    중복된 소켓 생성 로직 제거

---
+++ Common/Network/SocketBase.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace Common.Network
+{
+  public class SocketBase
+  {
+    protected Socket? socket;
+    protected IPEndPoint? endPoint;
+    protected Socket? CreateSocket(int port)
+    {
+      try
+      {
+        // 호스트 이름으로 IP 주소 가져오기
+        string host = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(host);
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint endPoint = new(ipAddr, port);
+        
+        // TCP 소켓 생성
+        socket = new(
+            addressFamily: ipAddr.AddressFamily,
+            socketType: SocketType.Stream,
+            protocolType: ProtocolType.Tcp
+        );
+        return socket;
+      }
+      catch (Exception ex)
+      {
+          Console.WriteLine($"소켓 생성 오류: {ex.Message}");
+          return null;
+      }
+    }
+  }
+}

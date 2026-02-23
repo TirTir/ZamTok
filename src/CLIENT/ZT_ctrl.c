a753b57e240777efed175b93ed0624a1427dc2f1
TirTir <uj0791navercom@skuniv.ac.kr>
2026-02-23 17:11:38 +0900

    [FEAT] stdin epoll 등록
    - EventLoop에 STDIN_FILENO 등록해 소켓 대기 중에도 입력 처리
    - 한 줄 입력 시 parse_cmd_line으로 파싱 후 CTRL_proc 디스패치

--- src/CLIENT/ZT_ctrl.c
+++ src/CLIENT/ZT_ctrl.c
@@ -1,7 +1,31 @@
-void CTRL_proc(void)
+#include "ZT_Inc.h"
+#include "ZT_ctrl.h"
+#include <stdio.h>
+#include <strings.h>
+
+void CTRL_proc(int argc, char **argv)
 {
-    if(!strcasecmp(argv[0], "member"))
-    {
-        
-    }
+	if (argc < 1)
+		return;
+
+	if (!strcasecmp(argv[0], "signup")) 
+	{
+		printf("[1] USER ID: ");
+		printf("[2] PASSWORD: ");
+		printf("[3] EMAIL: ");
+	} 
+	
+	if (!strcasecmp(argv[0], "help")) 
+	{
+		printf("Commands: help, member, quit\n");
+	}
+	
+	if (!strcasecmp(argv[0], "quit") || !strcasecmp(argv[0], "exit")) 
+	{
+		printf("[CTRL] quit\n");
+		exit(0);
+	} 
+	else {
+		printf("[CTRL] unknown command: %s (try 'help')\n", argv[0]);
+	}
 }
---
+++ src/CLIENT/ZT_ctrl.h
@@ -0,0 +1,7 @@
+#ifndef _ZT_CTRL_H_
+#define _ZT_CTRL_H_
+
+/* argc, argv: 터미널에서 입력한 한 줄을 공백 기준으로 나눈 결과 */
+void CTRL_proc(int argc, char **argv);
+
+#endif

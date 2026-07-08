bdb058fb19de95e2dda24af2c52352c00dde1dea
TirTir <uj0791navercom@skuniv.ac.kr>
2026-07-08 09:22:37 +0900

    save local changes before pull

--- src/SERVER/ZT_Inc.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ZT_INC_H_
-#define _ZT_INC_H_
-
-#include "zt_common.h"
-#include "zt_func.h"
-
-#endif
--- src/SERVER/ZT_queue.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ZT_QUEUE_H_
-#define _ZT_QUEUE_H_
-
-int QUEUE_Init();
-int Queue_Enqueue();
-void * Queue_Dequeue();
-void * Queue_Enqueue();
-
-#endif
